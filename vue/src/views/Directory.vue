e3037b8eee09abaea135345e4f4fc3709c4ffe3e
Bailey Uhl <baileyauhl@gmail.com>
2023-10-02 21:37:09 -0400

    directory and application button edits

--- vue/src/views/Directory.vue
+++ vue/src/views/Directory.vue
@@ -1,40 +1,47 @@
 <template>
   <div class="directory-container">
-    <div class="image-container">
-      <img src="../img/cartoon-background.jpg" alt="Cat and Dog" class="image-container">
-    </div>
-    <div class="overlay">
     <div class="container" style="margin-bottom: 64px;">
-     <div class="text">
+      <div class="text">
         Volunteer Directory
-     </div>
-     <table class="directory">
-         <thead>
-             <tr>
-                 <th>First Name</th>
-                 <th>Last Name</th>
-                 <th>Phone Number</th>
-                 <th>Email Address</th>
-             </tr>
-         </thead>
-         <tbody>
-          <tr v-for="user in users" :key="user.id">
-                <td>{{ user.firstName }}</td>
-                <td>{{ user.lastName }}</td>
-                <td>{{ user.phoneNumber }}</td>
-                <td>{{ user.emailAddress }}</td>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Phone Number</th>
+            <th scope="col">Email Address</th>
           </tr>
-         </tbody>
+        </thead>
+        <tbody>
+          <tr v-for="user in users" :key="user.id">
+            <td data-label="First Name">{{ user.firstName }}</td>
+            <td data-label="Last Name">{{ user.lastName }}</td>
+            <td data-label="Phone Number">{{ user.phoneNumber }}</td>
+            <td data-label="Email Address">{{ user.emailAddress }}</td>
+          </tr>
+        </tbody>
       </table>
-     </div>
+    </div>
+    <div class="background">
+      <img src="../img/1e4982cd-3f91-42c0-92d3-4f50f42496cb.png" alt="nonstretch">
+      <img src="../img/1e4982cd-3f91-42c0-92d3-4f50f42496cb.png" alt="2">
+      <img src="../img/1e4982cd-3f91-42c0-92d3-4f50f42496cb.png" alt="3">
+    </div>
+    <div class="background">
+      <img src="../img/1e4982cd-3f91-42c0-92d3-4f50f42496cb.png" alt="nonstretch">
+      <img src="../img/1e4982cd-3f91-42c0-92d3-4f50f42496cb.png" alt="2">
+      <img src="../img/1e4982cd-3f91-42c0-92d3-4f50f42496cb.png" alt="3">
+    </div>
   </div>
-  </div>
-  </template>
-  <script>
-  import ApprovedUsersService from '../services/ApprovedUsersService.js';
-  export default {
-    name: "directory",
-  data() {
+</template>
+
+<script>
+import ApprovedUsersService from '../services/ApprovedUsersService.js';
+
+export default {
+  name: "directory",
+data() {
   return {
     users: []
           };
@@ -43,143 +50,182 @@
     ApprovedUsersService.getApprovedUsers().then(
         (resp) => {
             this.users = resp.data;
-        }
-    );
-          },
-      };
-  </script>
-  <style scoped>
-  table.directory{
-    width: 100%;
-    border-collapse: collapse;
+        });
+      },
+    };
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
+
+*{
+  margin: 0;
+  padding: 0;
+  outline: none;
+  box-sizing: border-box;
+  font-family: 'Poppins', sans-serif;
+}
+
+body {
+  margin: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  padding: 10px;
+  font-family: 'Poppins', sans-serif;
+}
+
+.background{
+  display: flex;
+  flex-direction: column;
+}
+
+.directory-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: 'Poppins', sans-serif;
+  background-image: url(../img/1e4982cd-3f91-42c0-92d3-4f50f42496cb.png);
+}
+
+.container {
+  max-width: 60%;
+  width: 60%;
+  background-color: #FFFFFF;
+  padding: 40px 40px 40px 40px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+  border-radius: 8px;
+  margin: 0 auto;
+  margin-top: 60px; /* Adjust this value to control the vertical position of the form */
+  z-index: 1;
+}
+
+.text {
+  text-align: left;
+  font-size: 48px;
+  font-weight: 600;
+  font-family: 'Poppins', sans-serif;
+  color: #5E8558;
+  margin-left: 24px;
+  padding-bottom: 24px;
+}
+
+table {
+  border: 1px solid #ccc;
+  border-collapse: collapse;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  table-layout: fixed;
+}
+
+table caption {
+  font-size: 1.5em;
+  margin: .5em 0 .75em;
+}
+
+table tr {
+  background-color: #f8f8f8;
+  border: 1px solid #ddd;
+  padding: .35em;
+}
+
+table th,
+table td {
+  padding: .625em;
+  text-align: center;
+}
+
+table th {
+  font-size: .85em;
+  letter-spacing: .1em;
+  text-transform: uppercase;
+  background-color: #FFFFD1;
+}
+
+tbody {
+   word-wrap: break-word;
+}
+
+@media screen and (max-width: 600px) {
+  table {
+    border: 0;
   }
-  table.directory th, table.directory td{
-    border: 1px solid black;
-    padding: 8px;
-    text-align: center;
+
+  table caption {
+    font-size: 1.3em;
   }
-  @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
-  .image-container {
-    display: inline-block;
-    position: relative;
-    line-height: 0;
-    background: #FFFFFF;
-  }
-  .image-container::after {
-    content: '';
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    mix-blend-mode: multiply;
-    background: #FA00CC;
-    opacity: 0.5;
-  }
-  .image-container > img {
-    -webkit-filter:    grayscale(100%) hue-rotate(0deg) invert(0%) opacity(100%) saturate(100%) sepia(0%);
-            filter:    grayscale(100%) hue-rotate(0deg) invert(0%) opacity(100%) saturate(100%) sepia(0%);
-    mix-blend-mode: none;
-  }
-  .image-container {
-    width: 100%;
-    max-height: 600px;
+  
+  table thead {
+    border: none;
+    clip: rect(0 0 0 0);
+    height: 1px;
+    margin: -1px;
     overflow: hidden;
-    position: relative;
-  }
-  img {
-    width: 100%;
-    height: 500px;
-    object-fit: cover;
-  }
-  .overlay {
+    padding: 0;
     position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 1;
+    width: 1px;
   }
-  /* Long container: */
-  /* .container {
-    max-width: 60%;
-    width: 60%;
-    height: 1000px;
-    background-color: #FFFFFF;
-    padding: 64px 40px 32px 40px;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-    border-radius: 8px;
-    margin: 0 auto;
-    margin-top: 500px;
-    z-index: 1;
-  } */
-  /* Small container: */
+  
+  table tr {
+    border-bottom: 3px solid #ddd;
+    display: block;
+    margin-bottom: .625em;
+  }
+  
+  table td {
+    border-bottom: 1px solid #ddd;
+    display: block;
+    font-size: .8em;
+    text-align: right;
+  }
+  
+  table td::before {
+    content: attr(data-label);
+    float: left;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+  
+  table td:last-child {
+    border-bottom: 0;
+  }
+
+  th {
+    color: #8D6B94;
+  }
+}
+
+@media (max-width: 1000px) {
   .container {
-    max-width: 60%;
-    width: 60%;
-    background-color: #FFFFFF;
-    padding: 64px 40px 32px 40px;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-    border-radius: 8px;
-    margin: 0 auto;
-    margin-top: -275px;
+    max-width: 85%;
+    width: 85%;
+    padding: 16px 16px 16px 16px;
     z-index: 1;
   }
-  .text{
-    text-align: center;
+
+  .text {
     font-size: 24px;
-    font-weight: 600;
-    font-family: 'Poppins', sans-serif;
-    color: #5E8558;
-    padding-top: -50px;
-    margin-top: -35px;
-    margin-bottom: 30px;
+    text-align: left;
     line-height: 40px;
     margin-left: 8px;
-    /* outline: none; */
+    padding-bottom: 16px;
   }
-  @media (max-width: 700px) {
-      .container {
-      max-width: 85%;
-      width: 85%;
-      padding: 48px 16px 16px 16px;
-      margin-top: -450px;
-      z-index: 1;
-    }
-    .text{
-      font-size: 30px;
-      text-align: center;
-      line-height: 40px;
-      margin-left: 0;
-    }
-    table.directory {
-      text-align: center;
-      width: 100%;
-      border-collapse: collapse;
-      border-style: solid;
-    }
-  thead{
-      border: 1px solid black;
-      border-collapse: separate;
-      padding: 8px;
-      text-align: center;
+
+  table.directory {
+    text-align: center;
+    width: 100%;
+    border-collapse: collapse;
+    border-style: solid;
   }
-  tbody{
-      border: 1px solid black;
-      border-collapse: separate;
-      padding: 8px;
-      text-align: center;
-  }
-  table.directory th,
-  table.directory td {
-    outline-color: black;
-    outline: solid;
+
+  thead {
     border: 1px solid black;
+    border-collapse: separate;
     padding: 8px;
     text-align: center;
   }
-  }
-  </style>
+}
+</style>
